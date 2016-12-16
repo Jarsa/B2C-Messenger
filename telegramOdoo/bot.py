bf85c022a3b3d9c19012feba46cbdedfcd61e474
Hector-Camacho <hector.camacho.f@gmail.com>
2016-12-16 10:24:07 -0600

    [REF][Telebot] Added survey reponse in b2c messenger

--- telegramOdoo/bot.py
+++ telegramOdoo/bot.py
@@ -16,8 +16,10 @@ def comando_bienvenida(mensaje):
     itembtn2 = types.KeyboardButton('Realizar encuesta')
     itembtn3 = types.KeyboardButton('Solicitar factura')
     itembtn4 = types.KeyboardButton('Notificar irregularidad')
+    itembtn5 = types.KeyboardButton('Checar vencimiento de productos')
     markup.row(itembtn1, itembtn2)
     markup.row(itembtn3, itembtn4)
+    markup.row(itembtn5)
     tor.send_message(
         chat_id,
         'Hola! ¡Bienvenido a btoc messenger! Me llamo b-2 '
@@ -36,15 +38,21 @@ def solicitar_factura(message):
         'Al parecer tus datos no estan registrados en nuestro sistema.\n'
         '¿Te parece bien que comencemos el registro unico para guardar tu'
         ' informacion?', reply_markup=markup)
-    tor.register_next_step_handler(question, process_name_step)
+    tor.register_next_step_handler(question, process_confirmation_step)
+
+def process_confirmation_step(message):
+    chat_id = message.chat.id
+    if message.text == 'Si, claro que quiero!':
+        name = tor.send_message(chat_id, '¿A que nombre voy a facturar?')
+        tor.register_next_step_handler(name, process_name_step)
+    else:
+        tor.send_message(chat_id, 'Si quiere.')
 
 def process_name_step(message):
     chat_id = message.chat.id
-    if message.text == 'Si, claro que quiero!':
-        calle = tor.send_message(chat_id, '¿A que calle voy a facturar?')
-        tor.register_next_step_handler(calle, process_calle_step)
-    else:
-        tor.send_message(chat_id, 'Si quiere.')
+    name = message.text
+    calle = tor.send_message(chat_id, '¿A que calle voy a facturar?')
+    tor.register_next_step_handler(calle, process_calle_step)
 
 def process_calle_step(message):
     try:
@@ -115,8 +123,17 @@ def procces_codigo_postal_step(message):
     try:
         chat_id = message.chat.id
         codigo_postal = message.text
+        markup = types.ReplyKeyboardMarkup()
+        inciso_moral = types.KeyboardButton('Persona moral')
+        inciso_fisica = types.KeyboardButton('Persona fisica')
+        inciso_no_luc = types.KeyboardButton('Asociaciones no lucrativas')
+        inciso_sin_luc = types.KeyboardButton('Persona moral sin fin de lucro')
+        markup.row(inciso_moral)
+        markup.row(inciso_fisica)
+        markup.row(inciso_no_luc)
+        markup.row(inciso_sin_luc)
         regimen_fiscal = tor.send_message(
-            chat_id, '¿Cual es tu regimen fiscal?')
+            chat_id, '¿Cual es tu regimen fiscal?', reply_markup=markup)
         tor.register_next_step_handler(regimen_fiscal, process_regimen_fiscal)
     except Exception as e:
         tor.send_message(message, 'ooooops')
@@ -125,17 +142,8 @@ def process_regimen_fiscal(message):
     try:
         chat_id = message.chat.id
         regimen = message.text
-        razon_social = tor.send_message(chat_id, '¿Cual es tu razon social?')
-        tor.register_next_step_handler(razon_social, process_razon_social)
-    except Exception as e:
-        tor.send_message(message, 'ooooops')
-
-def process_razon_social(message):
-    try:
-        chat_id = message.chat.id
-        razon_social = message.text
-        rfc = tor.send_message(chat_id, '¿Cual es tu RFC?')
-        tor.register_next_step_handler(rfc, procces_rfc_step)
+        razon_social = tor.send_message(chat_id, '¿Cual es tu rfc?')
+        tor.register_next_step_handler(razon_social, procces_rfc_step)
     except Exception as e:
         tor.send_message(message, 'ooooops')
 
@@ -151,11 +159,58 @@ def procces_rfc_step(message):
         tor.send_message(message, 'ooooops')
 
 @tor.message_handler(func=lambda message: message.text == 'Checar mis puntos')
-def handle_text_doc(message):
+def handle_checar_puntos(message):
+    markup = types.ReplyKeyboardMarkup()
+    inciso_a = types.KeyboardButton('Si')
+    inciso_b = types.KeyboardButton('No')
+    inciso_c = types.KeyboardButton('Tal vez')
+    inciso_d = types.KeyboardButton('Prefiero no contestar')
+    markup.row(inciso_a, inciso_b)
+    markup.row(inciso_c, inciso_d)
     tor.send_message(
         message.chat.id,
         'Hola' + message.chat.first_name + ', actualmente cuentas con una'
         ' cantidad de 29 puntos, con los cuales puedes realizar compras en'
         ' alguno nuestros siguientes departamentos')
+
+@tor.message_handler(func=lambda message: message.text == 'Realizar encuesta')
+def handle_realizar_encuesta(message):
+    markup = types.ReplyKeyboardMarkup()
+    inciso_a = types.KeyboardButton('1')
+    inciso_b = types.KeyboardButton('2')
+    inciso_c = types.KeyboardButton('3')
+    inciso_d = types.KeyboardButton('Prefiero no contestar')
+    markup.row(inciso_a, inciso_b)
+    markup.row(inciso_c, inciso_d)
+    respuesta = tor.send_message(
+        message.chat.id,
+        'ENCUESTA DE SATISFACCION \nEstimado Cliente, rogamos su valiosa '
+        'ayuda para calificar el servicio que ha recibido del promotor que la '
+        'atiende en nuestra representación (Sr, Juan Martínez Pérez), en la '
+        'breve encuesta que le enviamos anexa en este mensaje, por favor solo '
+        'con el numero de la respuesta que elija.\n\n'
+        'El promotor, Sr Juan Martínez Pérez: \n1.- Acude puntualmente a las '
+        'reuniones, me proporciona toda la información que le solicito, me tra'
+        'ta amablemente \n2.- Acude a veces a las reuniones, hay información '
+        'que le solicite que no me da, a veces es amable. \n3.- Seguido no '
+        'acude a las reuniones, constantemente no me da la información que le '
+        'solicito, es grosero ', reply_markup=markup)
+    tor.register_next_step_handler(respuesta, process_guardar_respuesta)
+
+def process_guardar_respuesta(message):
+    try:
+        respuesta = message.text
+        if respuesta not in ['1', '2', '3']:
+            tor.send_message(
+                message.chat.id,
+                'Entendemos tu discrecion \n'
+                'Gracias por responder!')
+            pass
+        tor.send_message(
+            message.chat.id,
+            'Su respuesta fue: ' + respuesta + '\n Muchas gracias'
+            'por colaborar, seguiremos mejorando gracias a usted!')
+    except Exception as e:
+        tor.send_message(message.chat.id, 'oooooops')
 
 tor.polling(none_stop=True)
