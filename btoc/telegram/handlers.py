f834820b5c77313daa549abb50671c8b998845cb
Hector-Camacho <hector.camacho.f@gmail.com>
2016-12-22 10:14:47 -0600

    [ADD][Environment] Created enviroment object to manipulate odoo models from telegram handlers file

--- btoc/telegram/handlers.py
+++ btoc/telegram/handlers.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import openerp.tools.config as config
-from contextlib import closing
 import logging
-from openerp import api, SUPERUSER_ID, sql_db, models
+import openerp
+from openerp import api, SUPERUSER_ID
 from telebot import types
+from openerp.modules.registry import RegistryManager
+from openerp.api import Environment
+from contextlib import closing
 
 _logger = logging.getLogger("[TELEGRAM_HANDLERS]")
 
@@ -15,30 +17,42 @@ class TelegramBotHandlers(object):
         self.bot = bot
         self.uid = SUPERUSER_ID
         self.context = {}
+        self.partner = {
+            'name': '',
+            'street': '',
+            'l10n_mx_street3': '',
+            'l10n_mx_street4': '',
+            'street2': '',
+            'city_id': '',
+            'state_id': '',
+            'zip': '',
+            'l10n_mx_city2': '',
+            'regimen_fiscal_id': '',
+            'notify_email': 'always',
+            'company_type': 'person',
+            'active': True}
 
-    # def get_cursor(self):
-    #     return sql_db.db_connect(config['db_name']).cursor()
+    def show_telebot_menu(self):
+        markup = types.ReplyKeyboardMarkup()
+        itembtn1 = types.KeyboardButton('Solicitar factura')
+        itembtn2 = types.KeyboardButton('Notificacion de puntos ganados')
+        itembtn3 = types.KeyboardButton('Checar vencimiento de productos')
+        itembtn4 = types.KeyboardButton('Consulta de saldos')
+        itembtn5 = types.KeyboardButton('Notificar irregularidad')
+        itembtn6 = types.KeyboardButton('Realizar encuesta')
+        itembtn7 = types.KeyboardButton('Calificacion de servicios')
+        markup.row(itembtn1, itembtn2, itembtn3)
+        markup.row(itembtn4, itembtn5, itembtn6)
+        markup.row(itembtn7)
+        return markup
 
     def handle(self):
         _logger.info('TelegramBOTHandlers: handle -> %r' % (self.bot))
         BOT = self.bot
-        # env = api.Environment(
-        #     self.get_cursor(), self.uid, self.context)
-
+        markup = self.show_telebot_menu()
         @BOT.message_handler(commands=['start'])
         def comando_bienvenida(mensaje):
             chat_id = mensaje.chat.id
-            markup = types.ReplyKeyboardMarkup()
-            itembtn1 = types.KeyboardButton('Solicitar factura')
-            itembtn2 = types.KeyboardButton('Notificacion de puntos ganados')
-            itembtn3 = types.KeyboardButton('Checar vencimiento de productos')
-            itembtn4 = types.KeyboardButton('Consulta de saldos')
-            itembtn5 = types.KeyboardButton('Notificar irregularidad')
-            itembtn6 = types.KeyboardButton('Realizar encuesta')
-            itembtn7 = types.KeyboardButton('Calificacion de servicios')
-            markup.row(itembtn1, itembtn2, itembtn3)
-            markup.row(itembtn4, itembtn5, itembtn6)
-            markup.row(itembtn7)
             BOT.send_message(
                 chat_id,
                 'Hola! ¡Bienvenido a btoc messenger! Me llamo b-2 '
@@ -48,7 +62,7 @@ class TelegramBotHandlers(object):
         @BOT.message_handler(
             func=lambda message: message.text == 'Solicitar factura')
         def solicitar_factura(message):
-            markup = types.ReplyKeyboardMarkup()
+            markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
             affirmative = types.KeyboardButton('Si, claro que quiero!')
             negative = types.KeyboardButton('No, gracias!')
             markup.row(affirmative)
@@ -63,22 +77,11 @@ class TelegramBotHandlers(object):
 
         def process_confirmation_step(message):
             chat_id = message.chat.id
-            markup = types.ReplyKeyboardMarkup()
-            itembtn1 = types.KeyboardButton('Solicitar factura')
-            itembtn2 = types.KeyboardButton('Notificacion de puntos ganados')
-            itembtn3 = types.KeyboardButton('Checar vencimiento de productos')
-            itembtn4 = types.KeyboardButton('Consulta de saldos')
-            itembtn5 = types.KeyboardButton('Notificar irregularidad')
-            itembtn6 = types.KeyboardButton('Realizar encuesta')
-            itembtn7 = types.KeyboardButton('Calificacion de servicios')
-
-            markup.row(itembtn1, itembtn2, itembtn3)
-            markup.row(itembtn4, itembtn5, itembtn6)
-            markup.row(itembtn7)
+            markup = self.show_telebot_menu()
             if message.text == 'Si, claro que quiero!':
                 name = BOT.send_message(
                     chat_id,
-                    '¿A que nombre voy a facturar?')
+                    '¿Cual es tu nombre')
                 BOT.register_next_step_handler(name, process_name_step)
             else:
                 BOT.send_message(
@@ -86,32 +89,30 @@ class TelegramBotHandlers(object):
                     'cosa mas en la que pueda ayudarle?', reply_markup=markup)
 
         def process_name_step(message):
-            markup_hide = types.ReplyKeyboardRemove(selective=False)
             chat_id = message.chat.id
-            name = message.text
+            self.partner['name'] = message.text
             calle = BOT.send_message(
                 chat_id,
-                '¿A que calle voy a facturar?',
-                reply_markup=markup_hide)
+                '¿A que calle voy a facturar')
             BOT.register_next_step_handler(calle, process_calle_step)
 
         def process_calle_step(message):
             try:
                 chat_id = message.chat.id
-                calle = message.text
+                self.partner['street'] = message.text
                 num_int = BOT.send_message(
                     chat_id,
                     '¿Cual es el numero interior?')
                 BOT.register_next_step_handler(
-                    num_int, procces_numero_int_step)
+                    num_int, process_numero_int_step)
             except Exception as e:
                 BOT.send_message(message, 'ooooops')
 
-        def procces_numero_int_step(message):
+        def process_numero_int_step(message):
             try:
                 chat_id = message.chat.id
-                numero_int = message.text
-                if numero_int.isdigit() or numero_int.upper() == 'N/A':
+                self.partner['l10n_mx_street4'] = message.text
+                if message.text.isdigit() or message.text.upper() == 'N/A':
                     numero_ext = BOT.send_message(
                         chat_id,
                         '¿Cual es el '
@@ -126,15 +127,15 @@ class TelegramBotHandlers(object):
                         'numero. \n¿Cual es tu numero interior?')
                     BOT.register_next_step_handler(
                         numero_int,
-                        procces_numero_int_step)
+                        process_numero_int_step)
             except Exception as e:
                 BOT.send_message(message, 'ooooops')
 
         def process_numero_ext_step(message):
             try:
                 chat_id = message.chat.id
-                numero_ext = message.text
-                if not numero_ext.isdigit():
+                self.partner['l10n_mx_street3'] = message.text
+                if not message.text.isdigit():
                     numero_ext = BOT.send_message(
                         chat_id,
                         'El numero exterior debe de ser '
@@ -153,7 +154,7 @@ class TelegramBotHandlers(object):
         def process_colonia_step(message):
             try:
                 chat_id = message.chat.id
-                colonia = message.text
+                self.partner['street2'] = message.text
                 localidad = BOT.send_message(chat_id, '¿Cual es tu localidad?')
                 BOT.register_next_step_handler(
                     localidad, process_localidad_step)
@@ -163,7 +164,7 @@ class TelegramBotHandlers(object):
         def process_localidad_step(message):
             try:
                 chat_id = message.chat.id
-                localidad = message.text
+                self.partner['l10n_mx_city2'] = 1
                 ciudad = BOT.send_message(chat_id, '¿Cual es tu ciudad?')
                 BOT.register_next_step_handler(ciudad, process_ciudad_step)
             except Exception as e:
@@ -172,35 +173,35 @@ class TelegramBotHandlers(object):
         def process_ciudad_step(message):
             try:
                 chat_id = message.chat.id
-                colonia = message.text
+                self.partner['city_id'] = 1
                 estado = BOT.send_message(chat_id, '¿Cual es tu estado?')
-                BOT.register_next_step_handler(estado, procces_estado_step)
+                BOT.register_next_step_handler(estado, process_estado_step)
             except Exception as e:
                 BOT.send_message(message, 'ooooops')
 
-        def procces_estado_step(message):
+        def process_estado_step(message):
             try:
                 chat_id = message.chat.id
-                estado = message.text
+                self.partner['state_id'] = 1
                 codigo_postal = BOT.send_message(chat_id, '¿Cual es tu'
                     ' codigo postal?')
                 BOT.register_next_step_handler(
                     codigo_postal,
-                    procces_codigo_postal_step)
+                    process_codigo_postal_step)
             except Exception as e:
                 BOT.send_message(message, 'ooooops')
 
-        def procces_codigo_postal_step(message):
+        def process_codigo_postal_step(message):
             try:
                 chat_id = message.chat.id
-                codigo_postal = message.text
-                if not codigo_postal.isdigit():
+                self.partner['zip'] = message.text
+                if not message.text.isdigit():
                     codigo_postal = BOT.send_message(
                         chat_id,
                         'El codigo postal no tiene letras. '
                         '¿Cual es tu codigo postal?')
                     BOT.register_next_step_handler(
-                        codigo_postal, procces_codigo_postal_step)
+                        codigo_postal, process_codigo_postal_step)
                 else:
                     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
                     inciso_moral = types.KeyboardButton(
@@ -228,51 +229,43 @@ class TelegramBotHandlers(object):
             try:
                 markup_hide = types.ReplyKeyboardRemove(selective=False)
                 chat_id = message.chat.id
-                regimen = message.text
-                razon_social = BOT.send_message(
+                self.partner['regimen_fiscal_id'] = 1
+                rfc = BOT.send_message(
                     chat_id,
                     '¿Cual es tu rfc?',
                     reply_markup=markup_hide)
-                BOT.register_next_step_handler(razon_social, procces_rfc_step)
+                BOT.register_next_step_handler(rfc, process_rfc_step)
             except Exception as e:
                 BOT.send_message(message, 'ooooops')
 
-        def procces_rfc_step(message):
+        def process_rfc_step(message):
             try:
+                import ipdb; ipdb.set_trace()
                 chat_id = message.chat.id
                 letras = message.text[0:4]
                 numeros = message.text[4:10]
                 letras2 = message.text[10:12]
                 numeros2 = message.text[12]
-                rfc = message.text
+                self.partner['vat'] = 'MX' + message.text
                 if ((numeros.isdigit() and
                     numeros2.isdigit()) and
                     (isinstance(letras, basestring) and
                         isinstance(letras2, basestring))):
-                    markup = types.ReplyKeyboardMarkup()
-                    itembtn1 = types.KeyboardButton(
-                        'Solicitar factura')
-                    itembtn2 = types.KeyboardButton(
-                        'Notificacion de puntos ganados')
-                    itembtn3 = types.KeyboardButton(
-                        'Checar vencimiento de productos')
-                    itembtn4 = types.KeyboardButton(
-                        'Consulta de saldos')
-                    itembtn5 = types.KeyboardButton(
-                        'Notificar irregularidad')
-                    itembtn6 = types.KeyboardButton(
-                        'Realizar encuesta')
-                    itembtn7 = types.KeyboardButton(
-                        'Calificacion de servicios')
-                    markup.row(itembtn1, itembtn2, itembtn3)
-                    markup.row(itembtn4, itembtn5, itembtn6)
-                    markup.row(itembtn7)
-                    BOT.send_message(
-                        chat_id,
-                        'Excelente, has terminado '
-                        'de guardar tu informacion! ¿Alguna otra cosa en '
-                        'la que te pueda ayudar?',
-                        reply_markup=markup)
+                    markup = self.show_telebot_menu()
+                    with api.Environment.manage():
+                        r = RegistryManager.get('test')
+                        cr = r.cursor()
+                        Environment.reset()
+                        env = Environment(cr, SUPERUSER_ID, context={})
+                        with closing(env.cr):
+                            env['res.partner'].create(self.partner)
+                            BOT.send_message(
+                                chat_id,
+                                'Excelente, has terminado '
+                                'de guardar tu informacion! ¿Alguna'
+                                ' otra cosa en '
+                                'la que te pueda ayudar?',
+                                reply_markup=markup)
                 else:
                     BOT.send_message(
                         chat_id,
@@ -280,7 +273,7 @@ class TelegramBotHandlers(object):
                         'estar mal. Recuerda que primero son 4 digitos'
                         ' solamente, letras no')
             except Exception as e:
-                BOT.send_message(message, 'ooooops')
+                BOT.send_message(chat_id, 'ooooops')
 
         @BOT.message_handler(
             func=lambda message: message.text ==
@@ -298,7 +291,7 @@ class TelegramBotHandlers(object):
         @BOT.message_handler(
             func=lambda message: message.text ==
             'Checar vencimiento de productos')
-        def handle_realizar_encuesta(message):
+        def handle_consulta_saldo(message):
             BOT.send_message(
                 message.chat.id,
                 'Estimado ' + str(message.chat.first_name) + ', Ud '
@@ -328,7 +321,7 @@ class TelegramBotHandlers(object):
                 'acion digame como, cuando y donde fue que tuvo una regul'
                 'aridad dentro de nuestras tiendas')
             BOT.register_next_step_handler(
-                irregularidad, procces_notificar_irregularidad)
+                irregularidad, process_notificar_irregularidad)
 
         @BOT.message_handler(
             func=lambda message: message.text == 'Realizar encuesta')
@@ -365,7 +358,7 @@ class TelegramBotHandlers(object):
                 respuesta,
                 process_guardar_respuesta)
 
-        def procces_notificar_irregularidad(message):
+        def process_notificar_irregularidad(message):
             irregularidad = message.text
             BOT.send_message(
                 message.chat.id,
@@ -374,24 +367,7 @@ class TelegramBotHandlers(object):
 
         def process_guardar_respuesta(message):
             try:
-                markup = types.ReplyKeyboardMarkup()
-                itembtn1 = types.KeyboardButton(
-                    'Solicitar factura')
-                itembtn2 = types.KeyboardButton(
-                    'Notificacion de puntos ganados')
-                itembtn3 = types.KeyboardButton(
-                    'Checar vencimiento de productos')
-                itembtn4 = types.KeyboardButton(
-                    'Consulta de saldos')
-                itembtn5 = types.KeyboardButton(
-                    'Notificar irregularidad')
-                itembtn6 = types.KeyboardButton(
-                    'Realizar encuesta')
-                itembtn7 = types.KeyboardButton(
-                    'Calificacion de servicios')
-                markup.row(itembtn1, itembtn2, itembtn3)
-                markup.row(itembtn4, itembtn5, itembtn6)
-                markup.row(itembtn7)
+                markup = self.show_telebot_menu()
                 respuesta = message.text
                 if respuesta not in ['1', '2', '3']:
                     BOT.send_message(
